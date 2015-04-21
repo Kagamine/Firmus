29acac3f00bb3fe131ed14d5d9744f647b8ed740
あまみや ゆうこ <1@4321.io>
2015-04-21 17:04:09 +0800

    init

---
+++ models/mongodb.js
@@ -0,0 +1,16 @@
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://firmus:123456@localhost/firmus', {
+    server: {
+        auto_reconnect: true,
+        socketOptions:{
+            keepAlive: 1
+        }
+    },
+    db: {
+        numberOfRetries: 3,
+        retryMiliSeconds: 1000,
+        safe: true
+    }
+});
+GLOBAL.Types = mongoose.Types;
+exports.mongoose = mongoose;
