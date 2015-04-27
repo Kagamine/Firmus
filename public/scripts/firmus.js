c0a23544f65787e45ece43ed6a80777b8f8b3056
あまみや ゆうこ <1@4321.io>
2015-04-27 18:26:05 +0800

    pop msg

--- public/scripts/firmus.js
+++ public/scripts/firmus.js
@@ -9,3 +9,17 @@ $(window).resize(function () {
 $(document).ready(function () {
     resize();
 });
+
+function popResult(txt) {
+    var msg = $('<div class="msg hide">' + txt + '</div>');
+    msg.css('left', '50%');
+    $('body').append(msg);
+    msg.css('margin-left', '-' + parseInt(msg.outerWidth() / 2) + 'px');
+    msg.removeClass('hide');
+    setTimeout(function () {
+        msg.addClass('hide');
+        setTimeout(function () {
+            msg.remove();
+        }, 400);
+    }, 2600);
+}
