3552fa9a199cec54c4b4ef4e78b4d04304597bad
あまみや ゆうこ <1@4321.io>
2015-04-27 16:16:59 +0800

    news

--- controllers/general.js
+++ controllers/general.js
@@ -41,12 +41,50 @@ router.get('/news/:id', auth.checkRole('news', 'query'), function (req, res, nex
 router.post('/news/create', auth.checkRole('news', 'modify'), function (req, res, next) {
     let news = new db.news();
     news.title = '新建新闻';
-    news.content = '请在此处填写新闻内容';
+    news.content = '<p>请在此处填写新闻内容</p>';
     news.summary = '请在此处填写新闻内容';
     news.time = Date.now();
     news.save(function (err, news) {
         res.redirect('/general/news/edit/' + news._id);
     });
+});
+
+// 删除新闻
+router.post('/news/delete/:id', auth.checkRole('news', 'modify'), function (req, res, next) {
+    db.news.remove(req.params.id)
+        .exec()
+        .then(function () {
+            res.redirect('/general/news');
+        })
+        .then(null, next);
+});
+
+// 修改新闻
+router.get('/news/edit/:id', auth.checkRole('news', 'modify'), function (req, res, next) {
+    db.news.findById(req.params.id)
+        .select('_id title content')
+        .exec()
+        .then(function (news) {
+            res.render('general/newsEdit', { title: '编辑新闻', news: news });
+        })
+        .then(null, next);
+});
+
+// 修改文章
+router.post('/news/edit/:id', auth.checkRole('news', 'modify'), function (req, res, next) {
+    let summary = req.body.content.replace(/<[^>]+>/g, '');
+    if (summary.length >= 255)
+        summary = summary.substring(0, 247) + '...';
+    db.news.update({ _id: req.params.id }, {
+        title: req.body.title,
+        content: req.body.content,
+        sumamry: summary
+    })
+        .exec()
+        .then(function () {
+            res.redirect('/general/news/' + req.params.id);
+        })
+        .then(null, next);
 });
 
 module.exports = router;
