33e37a901ea01c213353acfdea29854af726fb68
applenele <1941973283@qq.com>
2015-07-07 15:56:49 +0800

    1

--- public/scripts/codecomb.droptxt.js
+++ public/scripts/codecomb.droptxt.js
@@ -39,6 +39,28 @@ function aselector(aid){
                 lock = false;
             });
         });
+
+        this.on('focus', function () {
+            if (lock) return;
+            lock = true;
+            var args = {};
+            args[field] = txt.val();
+            $('.codecomb-droptxt-outer').remove();
+            $.getJSON(url, args, function (data) {
+                var html = '<div class="codecomb-droptxt-outer" data-parent="' + selector + '">';
+                for (var i = 0; i < data.length && i < 10; i++) {
+                    html += '<div class="codecomb-droptxt-item" onclick="$(\'' + selector + '\').val($(this).text()); $(\'.codecomb-droptxt-outer\').remove()">' + data[i] + '</div>'
+                }
+                html += '</div>';
+                var dom = $(html);
+                $('body').prepend(dom);
+                dom.css('top', (txt.offset().top + txt.outerHeight()) + 'px');
+                dom.css('left', txt.offset().left);
+                dom.outerWidth(txt.outerWidth());
+                lock = false;
+            });
+        });
+
         $(document).on('click', function (e) {
             if ($(e).hasClass('codecomb-droptxt-outer')) return;
             if ($(e).parent('.codecomb-droptxt-outer').length > 0) return;
