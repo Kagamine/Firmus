f915aba6286a8c223a9b5ab8759d49e82dcd5e8b
applenele <1941973283@qq.com>
2015-07-08 10:33:44 +0800

    2

--- public/scripts/codecomb.droptxt.js
+++ public/scripts/codecomb.droptxt.js
@@ -72,20 +72,16 @@ function selector(aid){
             });
         });
 
-
-
         $(document).on('click', function (e) {
             if ($(e).hasClass('codecomb-droptxt-outer')) return;
             if ($(e).parent('.codecomb-droptxt-outer').length > 0) return;
+            if("#"+event.target.id == selector) return;
             $('div[data-parent="' + selector + '"]').slideUp(200);
             setTimeout(function () {
                 $('div[data-parent="' + selector + '"]').remove();
             }, 300);
         });
     }
-
-
-
 
     $.fn.dropGiftJson = function(url, field){
         var selector = this.selector;
@@ -115,6 +111,7 @@ function selector(aid){
         $(document).on('click', function (e) {
             if ($(e).hasClass('codecomb-droptxt-outer')) return;
             if ($(e).parent('.codecomb-droptxt-outer').length > 0) return;
+            if("#"+event.target.id == selector) return;
             $('div[data-parent="' + selector + '"]').slideUp(200);
             setTimeout(function () {
                 $('div[data-parent="' + selector + '"]').remove();
@@ -151,6 +148,7 @@ function selector(aid){
         $(document).on('click', function (e) {
             if ($(e).hasClass('codecomb-droptxt-outer')) return;
             if ($(e).parent('.codecomb-droptxt-outer').length > 0) return;
+            if("#"+event.target.id == selector) return;
             $('div[data-parent="' + selector + '"]').slideUp(200);
             setTimeout(function () {
                 $('div[data-parent="' + selector + '"]').remove();
@@ -183,9 +181,32 @@ function selector(aid){
             });
         });
 
+        this.on('focus', function () {
+            if (lock) return;
+            lock = true;
+            var args = {};
+            args[field] = txt.val();
+            $('.codecomb-droptxt-outer').remove();
+            $.getJSON(url, args, function (data) {
+                var html = '<div class="codecomb-droptxt-outer" data-parent="' + selector + '">';
+                for (var i = 0; i < data.length && i < 10; i++) {
+                    html += '<div class="codecomb-droptxt-item" onclick="$(\'' + selector + '\').val($(this).text()); $(\'.codecomb-droptxt-outer\').remove();selector($(this).next().val())">' + data[i].data + '</div><input type="hidden" value="'+data[i].id+'"/>'
+                }
+                html += '</div>';
+                var dom = $(html);
+                $('body').prepend(dom);
+                dom.css('top', (txt.offset().top + txt.outerHeight()) + 'px');
+                dom.css('left', txt.offset().left);
+                dom.outerWidth(txt.outerWidth());
+                lock = false;
+            });
+        });
+
+
         $(document).on('click', function (e) {
             if ($(e).hasClass('codecomb-droptxt-outer')) return;
             if ($(e).parent('.codecomb-droptxt-outer').length > 0) return;
+            if("#"+event.target.id == selector) return;
             $('div[data-parent="' + selector + '"]').slideUp(200);
             setTimeout(function () {
                 $('div[data-parent="' + selector + '"]').remove();
