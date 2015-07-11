506b66390b569c610f89aa36a4293a0f8fe2e877
yinyipeng <yin9964857@vip.qq.com>
2015-07-12 02:07:29 +0800

    2

--- public/scripts/firmus.js
+++ public/scripts/firmus.js
@@ -995,7 +995,20 @@ $(document).ready(function () {
     $('#txtCallOrderNumber').blur(function () {
         var number  = $('#txtCallOrderNumber').val();
        $.getJSON('/order/getOrderByNumber/'+number, function (data) {
-           console.log(data);
+           var str='';
+           str+='<span>订单号：'+data['number']+'</span><br/><span>价格：'+data['price']+'</span><br/><span>地址：' + data['address']['city']+data['address']['district']+data['address']['address']+'</span><br />';
+           str+='<span>订单类型：' + data['orderType'] +'</span><br /><span>订单时间：'+ moment(data['time']).format('YYYY-MM-DD')+'</span><br />';
+           str+='<span>业务员：'+data['user']['name']+'</span>';
+           str+='<table class="table-inline"><thead><tr class="tl"><th>品相</th><th>总瓶数</th><th>起送时间</th><th>配送方式</th><th>每次配送瓶数</th><th>剩余瓶数</th></tr></thead>';
+           for(var i=0;i<data.orders.length;i++){
+               str+='<tbody><tr><td>'+data.orders[i].milkType+'</td><td>'+data.orders[i].count+'</td><td>'+moment(data.orders[i].begin).format('YYYY-MM-DD')+'</td><td>'+data.orders[i].distributeMethod+'</td><td>'+data.orders[i].distributeCount+'</td><td>'+data.orders[i].leftCount+'</td></tr>'
+           }
+           str+='</tbody></table>';
+           console.log(str);
+           var dom = $(str);
+           dom.css('margin-left', -(dom.outerWidth() / 2));
+           $('body').append(dom);
+           dom.addClass('active');
        })
     });
 });
